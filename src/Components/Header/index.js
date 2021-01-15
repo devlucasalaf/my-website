ad18a9a46f846d9eec9bfcc4cb5ce1c3197c9700
devlucasalaf <68478705+devlucasalaf@users.noreply.github.com>
2021-01-15 15:01:11 -0300

    Fix include home link in header menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -16,9 +16,10 @@ const Header = () => {
               <div id={menuOpen === false ? 'bar3' : 'bar3-open'} className='menu-bar'></div>
             </div>
             <div id={menuOpen === false ? 'menu-closed' : 'menu-openned'} className='menu-wrapper'>
+            <a href='/' id={menuOpen === false ? 'home' : 'home-open'} className='menu-items'>Home</a>
               <a href='/sobre' id={menuOpen === false ? 'sobre' : 'sobre-open'} className='menu-items'>Sobre</a>
-              <a id={menuOpen === false ? 'projetos' : 'projetos-open'} className='menu-items'>Projetos</a>
-              <a id={menuOpen === false ? 'contato' : 'contato-open'} className='menu-items'>Contato</a>
+              <a href='/projetos' id={menuOpen === false ? 'projetos' : 'projetos-open'} className='menu-items'>Projetos</a>
+              <a href='/contato' id={menuOpen === false ? 'contato' : 'contato-open'} className='menu-items'>Contato</a>
             </div>
         </div>
     )
